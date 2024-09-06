bfbb7788c440424bbf92cad6b953dad4bb802860
Davi Saldanha <davisaldanha.ti@gmail.com>
2024-09-06 15:33:32 -0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const input = document.getElementById('uploadImage')
+const preview = document.getElementById('previewImage')
+
+input.addEventListener('change', () => {
+    const file = input.files[0]
+
+    if(file){
+        const reader = new FileReader()
+
+        reader.onload = (e) => {
+            preview.src = e.target.result   
+        }
+        reader.readAsDataURL(file)
+    }
+})
